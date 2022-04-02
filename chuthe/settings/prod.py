7bdbaca37510110c933b942effde4553094322e0
bboyadao <bboyadao@gmail.com>
2022-04-02 18:47:16 +0700

    update test database

--- chuthe/settings/prod.py
+++ chuthe/settings/prod.py
@@ -1,13 +1,12 @@
-from .commons import *
 import os
 
+from .commons import *
 
 DATABASE_NAME = os.getenv("DATABASE_NAME")
 DATABASE_URL = os.getenv("DATABASE_URL")
 DATABASE_PORT = os.getenv("DATABASE_PORT")
 DATABASE_USER = os.getenv("DATABASE_USER")
 DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
-
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = False
@@ -36,5 +35,8 @@ DATABASES = {
         "PASSWORD": DATABASE_PASSWORD,
         "HOST": DATABASE_URL,
         "PORT": DATABASE_PORT,
+        "TEST": {
+            "NAME": "testdb"
+        },
     }
 }
