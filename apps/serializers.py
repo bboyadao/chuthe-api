4f81cb335257d9746ec66a9e09fe221eebf265be
bboyadao <bboyadao@gmail.com>
2022-04-30 14:45:27 +0700

    done application
    done apps

--- apps/serializers.py
+++ apps/serializers.py
@@ -1,10 +1,20 @@
 from oauth2_provider.models import get_application_model
 from rest_framework import serializers
+from rest_framework.exceptions import APIException, NotFound
 
 
 class AppSer(serializers.ModelSerializer):
     class Meta:
         fields = '__all__'
-
         model = get_application_model()
 
+    def create(self, validated_data):
+        req = self.context.get("request")
+
+        if req:
+            if not req.user.is_anonymous:
+                validated_data["user"] = req.user
+            else:
+                raise NotFound(detail="Error user not found", code="404")
+            return super().create(validated_data)
+        raise APIException("Unknown Error")
