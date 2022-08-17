3ca341b96c31b27e05a086b4aa5e34938038c59b
bboyadao <bboyadao@gmail.com>
2022-08-18 03:38:56 +0700

    updated user to uuid

--- user/models.py
+++ user/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
+import uuid
 
 import zoneinfo
 from django.utils.translation import gettext_lazy as _
@@ -10,6 +11,7 @@ from django.conf import settings
 
 
 class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
     def __str__(self):
         return self.username
